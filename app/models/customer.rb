0d760e3d8aade7a25b3b48a5ffa4ef69004c9252
Felipe Roman <ftroman@miuandes.cl>
2020-04-09 18:30:22 -0400

    part1 ready

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,14 @@
 class Customer < ApplicationRecord
   has_many :orders
   has_many :tickets, through: :orders
+
+  validates :name, :lastname, presence: true
+  validates :email, presence: true, if: :valid_email?
+  validates :email, uniqueness: true
+
+  def valid_email?
+    email =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  end
+
 end
+
--- app/models/ticket_type.rb
+++ app/models/ticket_type.rb
@@ -1,3 +1,5 @@
 class TicketType < ApplicationRecord
   belongs_to :event
+
+  validates :price, :greater_than_or_equal_to => 0
 end
